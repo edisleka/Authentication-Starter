35a7f3359d462091cb5e5b54c48aabc92c6ebaff
Edis Leka <edisleka@yahoo.gr>
2025-04-06 15:19:53 +0300

    Sign in screen completed

--- src/app/(auth)/sign-in.tsx
+++ src/app/(auth)/sign-in.tsx
@@ -1,8 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, Pressable } from 'react-native'
+
 export default function SignInScreen() {
   return (
-    <View>
-      <Text>SignInScreen</Text>
+    <View className='flex-1 p-5 gap-5 justify-center bg-white'>
+      <Text className='text-2xl font-semibold'>Sign in</Text>
+      <TextInput
+        placeholder='Email'
+        className='border border-gray-300 p-2.5 rounded-md'
+      />
+      <TextInput
+        placeholder='Password'
+        className='border border-gray-300 p-2.5 rounded-md'
+        secureTextEntry={true}
+      />
+      <Pressable className='bg-[#4353FD] p-4 rounded-md items-center'>
+        <Text className='text-white text-base font-semibold'>Sign In</Text>
+      </Pressable>
     </View>
   )
 }
